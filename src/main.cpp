49ef0013845ff567d0bb1c7d4d1563b0e0c9e864
Alexou1209 <alexandredionne1209@gmail.com>
2025-11-24 10:44:23 -0500

    Fonction comm

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,16 @@ Date: 11/13/2025
 Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
 #include "main.h"
+#include "Communication.h"
 /* ****************************************************************************
 Fonctions d'initialisation (setup)
 **************************************************************************** */
+uint8_t trame[6] = {0,0,0,0,0,0};
+
 void setup()
 {
-
+    initUART1();
+    Serial.begin(9600);
 }
 
 /* ****************************************************************************
@@ -22,6 +26,11 @@ Fonctions de boucle infini (loop())
 **************************************************************************** */
 void loop()
 {
-
+    litUART(trame, 6);
+    Serial.print(trame[0]);
+    Serial.print(trame[1]);
+    Serial.print(trame[2]);
+    Serial.print(trame[3]);
+    Serial.println(trame[4]);
 }
 
